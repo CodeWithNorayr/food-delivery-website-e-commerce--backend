b74dba22420a6d780fef1b1c55e57a9764c48739
Norayr <norayr.babayann@gmail.com>
2026-02-19 15:23:37 +0400

    Update orderRoute.js

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -1,12 +1,17 @@
-import express from "express"
-import  { placeOrder,userOrders,verifyOrder,fetchAllOrders } from "../controllers/orderController.js"
-import authMiddleware from "../middleware/authMiddleware.js"
+import express from "express";
+import {
+  placeOrder,
+  userOrders,
+  verifyOrder,
+  fetchAllOrders
+} from "../controllers/orderController.js";
+import authMiddleware from "../middleware/authMiddleware.js";
 
-const orderRouter = express.Router()
+const orderRouter = express.Router();
 
-orderRouter.post('/place',authMiddleware,placeOrder)
-orderRouter.post('/verify',verifyOrder)
-orderRouter.post('/userorders',authMiddleware,userOrders)
-orderRouter.get('/allorders',fetchAllOrders)
+orderRouter.post('/place', authMiddleware, placeOrder);
+orderRouter.post('/verify', verifyOrder);
+orderRouter.post('/userorders', authMiddleware, userOrders);
+orderRouter.get('/allorders', authMiddleware, fetchAllOrders); // only admin can access
 
-export default orderRouter
+export default orderRouter;
